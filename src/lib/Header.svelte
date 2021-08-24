fbab8b7b9807a7fc788b8e69a8e24f4c61cf29a4
cepi herdiansyah <codeenma.projects@gmail.com>
2021-08-24 17:05:08 +0700

    udpate navbar

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -41,20 +41,28 @@
     let defaultIndex = 0
     let x , y
 
-    let isMobile = false
-      $: if(y <= 600){
-          isMobile = !isMobile
+    let isScroll = false
+      $: if(y > 150){
+          isScroll = true
+      } else {
+          isScroll = false
       }
 
 </script>
-<svelte:window bind:innerWidth={y}/>
+<svelte:window bind:scrollY={y}/>
 
-<div class="w-full border-b-2   text-white bg-transparent">
+<div class={`w-full border-b-2   bg-transparent ${isScroll === true ? "fixed bg-white text-black" : "text-white"}`}>
         <div class="container mx-auto flex ">
         <button class="px-3 py-1 block sm:hidden md:hidden" on:click={()=> handleOpenMenu = !handleOpenMenu}>
             <Icon data={bars} scale={1}/>
         </button> 
+        {#if isScroll === true}
+        <img src="//cdn.extramarks.id/web/images/website_v7/kelas-pintar-logo.png" alt="kelas pintar" class="h-8 px-4 self-center "> 
+        {:else}
         <img src="//cdn.extramarks.id/web/images/website_v7/kelas-pintar-white-logo.png" alt="kelas pintar" class="h-8 px-4 self-center "> 
+        {/if}
+        
+        
         <div class="hidden md:flex sm:flex  sm:block md:block">
             {#each MenuLists as item}
             <div class={`px-3 p-8 text-xs self-center ${item.title.toLowerCase() === "berlangganan" ? "bg-white text-yellow-600" : ""}`}>
